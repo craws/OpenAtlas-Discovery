d1d7074710a0e0b1a02534ac8c4f18d522d99010
Andi <andreas@olschnoegger.at>
2022-10-27 11:45:41 +0200

    add search

--- pages/data/index.vue
+++ pages/data/index.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import headers from "../../assets/tableheaders.json";
 import classes from "../../assets/classes.json";
+import { Query } from "~~/types/query";
 const { $api } = useNuxtApp();
 
-
-const { data, pending, error, refresh } = await useAsyncData(() => $api.query.queryList({
-    view_classes: ["actor", "event", "place", "reference", "source"],
-    page: options.page,
-    limit: options.itemsPerPage
-}));
 const itemsLength = computed(() => data?.value?.data.pagination.entities);
 const options = reactive({ page: 1, itemsPerPage: 20, itemsLength })
+const query = ref({
+    view_classes: ["actor", "event", "place", "reference", "source"],
+    page: options.page,
+    limit: options.itemsPerPage,
+    search:undefined
+})
+
+const { data, pending, error, refresh } = await useAsyncData(() => $api.query.queryList(query.value));
+
 watch(() => options.page, () => refresh());
 
 onMounted(() => {
@@ -24,26 +28,35 @@ const icons = {
     acquisition: "mdi-calendar",
 
 }
+
+function updateQuery(newQuery: Query){
+    console.log(newQuery)
+    query.value.search=newQuery.search.map(x => JSON.stringify(x));
+    refresh();
+}
 </script>
 
 <template>
-    <data-table :loading="pending" height="calc(100vh - 64px)" density="compact" :headers="headers"
-        :items="data?.data?.results ?? []" :options="options">
-        <template #features[0].systemClass="{ value }">
-            <v-tooltip :text="$t(value)">
-                <template v-slot:activator="{ props }">
-                    <v-icon v-bind="props">{{ classes.find(x => x.systemClass === value)?.icon }}</v-icon>
-                </template>
-            </v-tooltip>
-        </template>
-        <template #features[0].properties.title="{ item, value }">
-            <nuxt-link :to="`/entity/${item.features[0]['@id'].split('/').at(-1)}`">{{ value }} </nuxt-link>
-        </template>
-        <template #features[0].when.timespans[0].start.earliest="{ value }">
-            {{ useFormatDateTime(value) }}
-        </template>
-        <template #features[0].when.timespans[0].end.earliest="{ value }">
-            {{ useFormatDateTime(value) }}
-        </template>
-    </data-table>
+    <v-container>
+        <search @search="updateQuery"></search>
+        <data-table :loading="pending" height="calc(100vh - 64px)" density="compact" :headers="headers"
+            :items="data?.data?.results ?? []" :options="options">
+            <template #features[0].systemClass="{ value }">
+                <v-tooltip :text="$t(value)">
+                    <template v-slot:activator="{ props }">
+                        <v-icon v-bind="props">{{ classes.find(x => x.systemClass === value)?.icon }}</v-icon>
+                    </template>
+                </v-tooltip>
+            </template>
+            <template #features[0].properties.title="{ item, value }">
+                <nuxt-link :to="`/entity/${item.features[0]['@id'].split('/').at(-1)}`">{{ value }} </nuxt-link>
+            </template>
+            <template #features[0].when.timespans[0].start.earliest="{ value }">
+                {{ useFormatDateTime(value) }}
+            </template>
+            <template #features[0].when.timespans[0].end.earliest="{ value }">
+                {{ useFormatDateTime(value) }}
+            </template>
+        </data-table>
+    </v-container>
 </template>
--- pages/map/index.vue
+++ pages/map/index.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts">
+<script setup lang="ts">import { Query } from '~~/types/query';
+
 const { $api } = useNuxtApp();
-const { data, pending, error, refresh } = await useAsyncData(() => $api.geometricEntities.geometricEntitiesList());
-onMounted(() => refresh());
+
+const query = ref({
+    view_classes: ["actor", "event", "place", "reference", "source"],
+    limit:0,
+    show:'geometry',
+    format:'geojson',
+    search:undefined
+})
+const { data, pending, error, refresh } = await useAsyncData(() => $api.query.queryList(query.value));
+onMounted(() => {
+    refresh();
+});
 
 interface CardContent {
     title: string
@@ -10,19 +21,25 @@ interface CardContent {
     text?: string
 }
 const cardContent = reactive<CardContent>({ title: '' })
-
+const items = computed(() => data?.value?.data?.results?.[0].features || [])
 function handlePopup(e: L.LeafletMouseEvent) {
+    console.log(e)
     cardContent.title = e?.target?.feature?.properties?.name ?? ''
     cardContent.subtitle = e?.target?.feature?.properties?.objectType
-    cardContent.id = e?.target?.feature?.properties?.objectId;
-    cardContent.text = e?.target?.feature?.properties?.objectDescription
+    cardContent.id = e?.target?.feature?.properties?.['@id'];
+    cardContent.text = e?.target?.feature?.geometry?.description
+}
+
+function updateQuery(newQuery: Query){
+    query.value.search=newQuery.search.map(x => JSON.stringify(x));
+    refresh();
 }
 </script>
 <template>
     <div>
         <div style="position:relative; overflow: hidden;">
-            <v-card width="300px" class="popup" :class="{ move: !cardContent.id }" position="absolute" top="10px"
-                style="right:20px; top:20px;z-index:500">
+            <search class="search" @search="updateQuery"></search>
+            <v-card width="300px" class="popup" :class="{ move: !cardContent.id }" position="absolute">
                 <v-card-title>
                     <div class="d-flex justify-space-between">
                         <p class="text-wrap">
@@ -34,7 +51,7 @@ function handlePopup(e: L.LeafletMouseEvent) {
                 <v-card-subtitle>
                     <span>{{ cardContent.subtitle }}</span>
                 </v-card-subtitle>
-                <v-card-text style="max-height:70vh;white-space: pre-line" class="scroll">
+                <v-card-text style="max-height:calc(100vh - 320px);white-space: pre-line" class="scroll">
                     <div>
                         {{ cardContent.text }}
                     </div>
@@ -43,7 +60,7 @@ function handlePopup(e: L.LeafletMouseEvent) {
                     <v-btn :to="`/entity/${cardContent.id}`" variant="text">{{ $t('more details') }}</v-btn>
                 </v-card-actions>
             </v-card>
-            <data-map @item-clicked="handlePopup" :items="data?.data?.features || []"
+            <data-map @item-clicked="handlePopup" :items="items"
                 style="height:calc(100vh - 64px);">
             </data-map>
 
@@ -56,7 +73,7 @@ function handlePopup(e: L.LeafletMouseEvent) {
 .popup {
     transition: all 100ms ease-in-out;
     right: 20px;
-    top: 20px;
+    top: 100px;
     z-index: 500
 }
 
@@ -66,5 +83,15 @@ function handlePopup(e: L.LeafletMouseEvent) {
 
 .scroll {
     overflow-y: auto
+}
+
+.search{
+    position:absolute;
+    z-index:500;
+    width: 60%;
+    left:0;
+    right:0;
+    margin:auto;
+    top:20px
 }
 </style>
