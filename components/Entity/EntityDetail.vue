2613c0d5271cdf6056b06ac8651c5c57de965919
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2022-11-08 15:20:12 +0100

    Better Expansion for Entities with lot's of relations

--- components/Entity/EntityDetail.vue
+++ components/Entity/EntityDetail.vue
@@ -7,34 +7,52 @@
     </v-card-title>
 
 
-    <v-expand-transition>
-      <div v-show="showDetails">
-        <v-divider></v-divider>
+    <v-divider/>
+    <v-card-text class="mb-0 pb-0">
+      <v-list>
+        <template v-for="(item, index) in details">
+          <template v-if="item && index < 3">
+            <v-divider v-if="index > 0"/>
+            <p
+            :key="item.label">
+              <nuxt-link :to="`/entity/${item.relationTo.split('/').at(-1)}`">{{ item.label }} </nuxt-link>
+            </p>
+          </template>
+        </template>
 
-        <v-card-text>
-          <v-list>
-            <template v-for="(item, index) in details">
-              <v-subheader
-                v-if="item"
-                :key="item.label"
-              >
-                <nuxt-link :to="`/entity/${item.relationTo.split('/').at(-1)}`">{{ item.label }} </nuxt-link>
-              </v-subheader>
+        <div v-show="!showDetails && showExpandable">
+          <v-divider/>
+          <p>... {{ details.length - 3}} {{ $t('components.entity.detail.more')}} </p>
+        </div>
 
-              <v-divider/>
-
-
+        <v-expand-transition>
+          <div v-show="showDetails">
+            <template v-for="(item, index) in details" >
+              <template v-if="item && index >= 3">
+                <v-divider/>
+                <p
+                :key="item.label">
+                  <nuxt-link :to="`/entity/${item.relationTo.split('/').at(-1)}`">{{ item.label }} </nuxt-link>
+                </p>
+              </template>
             </template>
-          </v-list>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+          </div>
+        </v-expand-transition>
+      </v-list>
+    </v-card-text>
 
+
+
+    <v-divider v-if="showExpandable" v-show="!showDetails" class="closed-divider" transition="fade-transition"/>
     <v-card-actions>
+      <v-spacer/>
       <v-btn
-          :icon="showDetails ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-          @click="showDetails = !showDetails"
+        v-if="showExpandable"
+        variant="flat"
+        :icon="showDetails ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+        @click="showDetails = !showDetails"
       ></v-btn>
+      <v-spacer/>
     </v-card-actions>
   </v-card>
 
@@ -51,7 +69,16 @@ const props = defineProps({
   }
 })
 
-const showDetails = ref(true);
+const showDetails = ref(false);
+
+const showExpandable = computed(() => {
+  if(props.details && Array.isArray(props.details) && props.details.length > 2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+})
 
 onMounted(
   () =>{
@@ -60,3 +87,9 @@ onMounted(
 )
 
 </script>
+<style scoped>
+.closed-divider {
+  transform: translate(0px,30px);
+}
+
+</style>
