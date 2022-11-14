5cde1f272503cc0d555b7e6cc0ef2eb5988e2896
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2022-11-14 11:41:14 +0100

    Moved When Display to own Component

--- components/Entity/EntityBasics.vue
+++ components/Entity/EntityBasics.vue
@@ -15,17 +15,9 @@
                   {{ systemClassName }}
                 </p>
           </div>
-          <v-row>
-            <v-col>
-              <p> {{$t('components.entity.basics.start')}}: {{ start }} </p>
-            </v-col>
-            <v-spacer/>
-            <v-col>
-              <p> {{$t('components.entity.basics.end')}}: {{ end }} </p>
-            </v-col>
-            <v-spacer/>
-            <v-spacer/>
-          </v-row>
+          <WhenDisplay
+            :loading="props.loading"
+            :when="props.when"></WhenDisplay>
         </div>
         <v-content v-for="(description, index) in descriptions">
           <v-divider v-if="index > 0"></v-divider>
@@ -42,15 +34,9 @@ import { useI18n } from 'vue-i18n';
 import classes from "../../assets/classes.json";
 import type { DescriptionModel, WhenModel } from '~~/composables/api';
 
-
 const { t } = useI18n();
 
 interface displayOptions {
-  time: {
-    showTime?: boolean,
-    showSymbolForMissingDate?: boolean,
-    showUnknownIfMissing: boolean,
-  }
 }
 
 interface Props {
@@ -73,71 +59,6 @@ const classIcon = computed(() => {
     return (classes.find(x => x.crmClass === crmClass.value)?.icon) ?? 'mdi-help';
   }
 });
-
-
-interface Moment {
-  date?: {
-    year?: string,
-    month?: string,
-    day?: string,
-  }
-  time?: {
-    hour?: string,
-    minute?: string,
-    second?: string,
-  }
-}
-
-function convertStringToDate(dateStr?:string) {
-  if(!dateStr) {
-    return null;
-  }
-
-  console.log('test');
-
-  const moment: Moment = {};
-
-  const jsDate = new Date()
-
-  const datePart = dateStr.split('T')[0].split('-');
-  const time = dateStr.split('T')[1].split(':');
-
-  console.log(datePart);
-  console.log(time);
-
-  moment.date.year = datePart[0];
-  moment.date.month = datePart[1];
-  moment.date.day = datePart[2];
-
-  moment.time.hour = time[0];
-  moment.time.minute = time[1];
-  moment.time.second = time[2];
-
-  return moment;
-
-  //  return new Date(datePart[0], datePart[1], datePart[2], time[0], time[1], time[2]);
-}
-
-function getStringDateFromMoment(moment: Moment, seperator?: string, format?:string){
-  let dateString = '';
-
-  return moment.date.year;
-}
-
-const start = computed(() => {
-  const timespan = props?.when?.timespans[0];
-  if(!timespan || !timespan.start){
-    return t('global.basics.unknown');
-  }
-  return (timespan.start.earliest ?? '?') + ' - ' + (timespan.start.latest ?? '?');
-} )
-const end = computed(() => {
-  const timespan = props?.when?.timespans[0];
-  if(!timespan || !timespan.end){
-    return t('global.basics.unknown');
-  }
-  return (timespan.end.earliest ?? '?') + ' - ' + (timespan.end.latest ?? '?');
-})
 
 
 const crmClass = computed(() => props.systemClass.substring(props.systemClass.indexOf(':') + 1, props.systemClass.indexOf(' ')))
