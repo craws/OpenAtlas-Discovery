cd32097731818889b38ddb1e4e17b476235ce84e
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2022-11-15 11:51:25 +0100

    Removed unused import and variable, Clarified source of Data

--- components/Entity/EntityMap.vue
+++ components/Entity/EntityMap.vue
@@ -2,7 +2,7 @@
     <v-card
     class="mx-auto my-6"
     style="min-width: 400px; min-height: 400px; max-width: 1000px; max-height:750;">
-        <data-map :items="geoData" style="min-width: 400px; min-height: 400px; max-width: 1000px; max-height:500;"></data-map>
+        <data-map :items="props.geoData" style="min-width: 400px; min-height: 400px; max-width: 1000px; max-height:500;"></data-map>
         <v-card-actions justify="center">
             <v-spacer/>
             <v-btn to="/map" prepend-icon="mdi-map-marker">
@@ -13,11 +13,8 @@
     </v-card>
 </template>
 <script lang="ts" setup>
-import { useI18n } from 'vue-i18n';
 import { GeometryModel } from '~~/composables/api';
 const props = defineProps<{
     geoData: GeometryModel;
 }>();
-
-const geoJson = useMapGeometryModelToGeoJsonObject(props.geoData);
 </script>
