99e52ee899bdda71f687c0dd4cb885ff604f38d9
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2022-12-13 15:37:20 +0100

    Fixed Data Page not working on direct load

--- pages/data/index.vue
+++ pages/data/index.vue
@@ -33,36 +33,38 @@ useHead({
 </script>
 
 <template>
-  <v-container>
-    <search-field :loading="pending" @search="updateQuery" />
-    <data-table
-      class="mt-10"
-      height="calc(100vh - 150px)"
-      density="compact"
-      :headers="headers"
-      :items="data?.results ?? []"
-      :options="options"
-    >
-      <template #features[0].systemClass="{ value }">
-        <v-tooltip content-class="text-capitalize" :text="$t(`global.entity.system_classes.${value}`)">
-          <template #activator="{ props }">
-            <v-icon v-bind="props">
-              {{ classes.find(x => x.systemClass === value)?.icon }}
-            </v-icon>
-          </template>
-        </v-tooltip>
-      </template>
-      <template #features[0].properties.title="{ item, value }">
-        <nuxt-link :to="`/entity/${item.features[0]['@id'].split('/').at(-1)}`">
-          {{ value }}
-        </nuxt-link>
-      </template>
-      <template #features[0].when.timespans[0].start.earliest="{ value }">
-        {{ useFormatDateTime(value) }}
-      </template>
-      <template #features[0].when.timespans[0].end.earliest="{ value }">
-        {{ useFormatDateTime(value) }}
-      </template>
-    </data-table>
-  </v-container>
+  <ClientOnly>
+    <v-container>
+      <search-field :loading="pending" @search="updateQuery" />
+      <data-table
+        class="mt-10"
+        height="calc(100vh - 150px)"
+        density="compact"
+        :headers="headers"
+        :items="data?.results ?? []"
+        :options="options"
+      >
+        <template #features[0].systemClass="{ value }">
+          <v-tooltip content-class="text-capitalize" :text="$t(`global.entity.system_classes.${value}`)">
+            <template #activator="{ props }">
+              <v-icon v-bind="props">
+                {{ classes.find(x => x.systemClass === value)?.icon }}
+              </v-icon>
+            </template>
+          </v-tooltip>
+        </template>
+        <template #features[0].properties.title="{ item, value }">
+          <nuxt-link :to="`/entity/${item.features[0]['@id'].split('/').at(-1)}`">
+            {{ value }}
+          </nuxt-link>
+        </template>
+        <template #features[0].when.timespans[0].start.earliest="{ value }">
+          {{ useFormatDateTime(value) }}
+        </template>
+        <template #features[0].when.timespans[0].end.earliest="{ value }">
+          {{ useFormatDateTime(value) }}
+        </template>
+      </data-table>
+    </v-container>
+  </ClientOnly>
 </template>
