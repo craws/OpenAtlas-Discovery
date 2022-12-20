892eadc62c156b21d820bb1f5b21bd4192de1b42
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2022-12-20 16:35:37 +0100

    Moved to main Nuxt-Test release

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@
 
 // Configure Vitest (https://vitest.dev/config/)
 
-import { defineConfig } from "vitest/config";
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
+  plugins: [vue()],
   test: {
     deps: {
-      inline: [/@nuxt\/test-utils-edge/],
+      inline: [/@nuxt\/test-utils/]
     },
-  },
-})
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      exclude: [
+        'node_modules/'
+      ]
+    }
+  }
+});
