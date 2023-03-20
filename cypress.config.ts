e17827475ca81de1e8aca93c1cbe8d7067f27474
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2023-03-20 17:14:31 +0100

    Undo Changes to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.PUBLIC_URL || 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000'
   }
 });
