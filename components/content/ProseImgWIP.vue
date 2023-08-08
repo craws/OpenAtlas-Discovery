209c90e904c16ea20187c4aba75181aa0c20126d
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2023-08-08 18:52:05 +0200

    Stopped using the nuxt image for nuxt content Nuxt Image for static sites is still in development. It was causing issues with NuxtContent, and therefore disabled for now.

--- components/content/ProseImgWIP.vue
+++ components/content/ProseImgWIP.vue
@@ -1,7 +1,12 @@
+<!--
+  The goal is to use this for static site genaration using nuxt image (https://image.nuxtjs.org/advanced/static-images).
+  However, at the moment that feature of nuxt image is still under development for Nuxt 3.
+  Therefore, this file is not used at the moment. To use it, rename it to ProseImg.vue.
+-->
+
 <template>
   <nuxt-img :src="src" :alt="alt" :width="width" :height="height" />
 </template>
-
 <script setup lang="ts">
 defineProps({
   src: {
