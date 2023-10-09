e987d9b52ff0bb382a00bff9e6f3ca5610117f99
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2023-10-09 13:29:29 +0200

    Added basic impressum

---
+++ pages/impressum.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ma-16 ">
+    <h1>{{ $t('global.imprint') }}</h1>
+    <div v-html="data" />
+  </div>
+</template>
+<script setup lang="ts">
+const imprintServiceURL = 'https://imprint.acdh.oeaw.ac.at/';
+const serviceID = '21614';
+const { locale } = useI18n();
+
+const { data } = await useFetch(imprintServiceURL + serviceID + `/?locale=${locale.value}`);
+
+</script>
