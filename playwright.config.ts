f983c3baf1ef58691bdf64b3dc09ffed1099674d
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-01-24 13:31:21 +0100

    chore: use dotenv-expand

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig, devices } from "@playwright/test";
-import { config } from "dotenv";
+import { join } from "node:path";
 
-config();
+import { defineConfig, devices } from "@playwright/test";
+import { config as dotenv } from "dotenv";
+import { expand } from "dotenv-expand";
+
+for (const envFilePath of [".env.test.local", ".env.local", ".env.test", ".env"]) {
+	expand(dotenv({ path: join(process.cwd(), envFilePath) }));
+}
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -65,9 +70,9 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+		webServer: {
+	  command: 'npm run start',
+	  url: 'http://127.0.0.1:3000',
+	  reuseExistingServer: !process.env.CI,
+	 },
 });
