086485429eb5353dc41bb490792f59bd794674f3
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-02-06 18:13:05 +0100

    Add fallback for missing descriptions

--- components/entity-descriptions-display.vue
+++ components/entity-descriptions-display.vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
 
-defineProps<{
+const t = useTranslations();
+
+const props = defineProps<{
 	descriptions: Array<{ value?: string }>;
 }>();
+
+const filteredDescriptions = computed(() => {
+	return props.descriptions.filter((description: { value?: string }) => { return description.value });
+});
 
 </script>
 
 <template>
-	<div v-for="(description, index) in descriptions" :key="description.value ?? 'descr' + index">
-		<template v-if="description.value">
-			<VDivider v-if="index > 0" />
-			<p class="text-md">
-				{{ description.value }}
-			</p>
-		</template>
-	</div>
+	<template v-if="filteredDescriptions.length > 0">
+		<div v-for="(description, index) in filteredDescriptions" :key="description.value ?? 'descr' + index">
+			<template v-if="description.value">
+				<VDivider v-if="index > 0" />
+				<p class="text-md">
+					{{ description.value }}
+				</p>
+			</template>
+		</div>
+	</template>
+	<template v-else>
+		<p class="text-md">
+			{{ t("EntityDescriptionsDisplay.no-descriptions") }}
+		</p>
+	</template>
 </template>
