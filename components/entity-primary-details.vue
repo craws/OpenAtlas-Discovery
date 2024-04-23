8aebe8a7ddb0bacf2058ce4d62d822a0f611f4bd
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-04-23 16:01:59 +0200

    Extract Primary Details Component

---
+++ components/entity-primary-details.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import CustomPrimaryDetailsActor } from '@/components/custom-primary-details-actor.vue';
+
+
+const props = defineProps<{
+	entity: EntityFeature,
+}>();
+
+const images = computed(() => {
+	return props.entity.depictions
+		?.reduce((acc: Array<{url: string, license:string}>, depiction) => {
+			if (depiction.url && depiction.license) {
+				acc.push({
+					url: depiction.url,
+					license: depiction.license
+				});
+			}
+			return acc;
+		}, []);
+});
+
+const customPrimaryDetails = computed(() => {
+	return entityPrimaryDetailsDict[props.entity.systemClass];
+});
+
+const entityPrimaryDetailsDict: Record<string, Component> = {
+}
+
+</script>
+
+<template>
+	<Card class="h-full">
+			<CardHeader>
+				<EntitySystemClass :system-class="entity.systemClass" />
+				<PageTitle>{{ entity.properties.title }}</PageTitle>
+				<EntityAliases v-if="entity.names" :aliases="entity.names" />
+				<EntityTimespans :timespans="entity.when?.timespans" />
+			</CardHeader>
+			<CardContent>
+				<div class="grid gap-4">
+					<EntityDescriptions :descriptions="entity?.descriptions ?? []" />
+
+					<component :is="customPrimaryDetails" v-if="customPrimaryDetails" :entity="entity" />
+
+					<!-- Types -->
+					<div class=" flex flex-row flex-wrap gap-1">
+						<TypesPopover
+							v-for="type in entity.types"
+							:key="(type.identifier ?? type.label) ?? 'missing'"
+							:type="type" />
+					</div>
+					<EntityImages v-if="images" :images="images" class="overflow-hidden"/>
+				</div>
+			</CardContent>
+		</Card>
+</template>
--- components/entity-sidebar.vue
+++ components/entity-sidebar.vue
@@ -1,25 +1,9 @@
 <script setup lang="ts">
-
-
 import {ChevronLeftIcon,ChevronRightIcon } from "lucide-vue-next"
 
 const t = useTranslations();
 
-const props = defineProps<{entity: EntityFeature}>();
-
-const images = computed(() => {
-	return props.entity.depictions
-		?.reduce((acc: Array<{url: string, license:string}>, depiction) => {
-			if (depiction.url && depiction.license) {
-				acc.push({
-					url: depiction.url,
-					license: depiction.license
-				});
-			}
-			return acc;
-		}, []);
-
-});
+defineProps<{entity: EntityFeature}>();
 
 </script>
 
@@ -31,33 +15,10 @@ const images = computed(() => {
 			<span class="sr-only">{{ t("EntityPage.sidebar.toggle", {title: entity.properties.title}) }}</span>
 		</summary>
 
-		<!-- START: <entity-primary-details/> -->
-		<Card class="h-full">
-			<CardHeader>
-				<EntitySystemClass :system-class="entity.systemClass" />
-				<PageTitle>{{ entity.properties.title }}</PageTitle>
-				<EntityAliases v-if="entity.names" :aliases="entity.names" />
-				<EntityTimespans :timespans="entity.when?.timespans" />
-			</CardHeader>
-			<CardContent>
-				<div class="grid gap-4">
-					<EntityDescriptions :descriptions="entity?.descriptions ?? []" />
-					<slot name="custom-primary-details" />
-					<!-- Types -->
-					<div class=" flex flex-row flex-wrap gap-1">
-						<TypesPopover
-							v-for="type in entity.types"
-							:key="(type.identifier ?? type.label) ?? 'missing'"
-							:type="type" />
-					</div>
-					<EntityImages v-if="images" :images="images" class="overflow-hidden"/>
-				</div>
-			</CardContent>
-		</Card>
-		<!-- END: <entity-primary-details/> -->
+		<EntityPrimaryDetails :entity="entity" />
 
 
-		<!-- <slot name="custom-details" /> -->
+		<slot name="custom-details" />
 		<!-- <component v-if="hasCustomDetails" v-bind:is="entityDetailsDict" bind:entity-data /> -->
 
 
