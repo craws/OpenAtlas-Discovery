8e7fe092654e1f30d368b10116732ae6b83a5745
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-04-29 14:06:58 +0200

    feat: Actor Relations Collapsible

--- components/actor-relation-collapsible.vue
+++ components/actor-relation-collapsible.vue
@@ -1,17 +1,13 @@
 <script lang="ts" setup>
-import { ChevronsUpDown } from 'lucide-vue-next';
+import { groupByToMap } from "@acdh-oeaw/lib";
+import { ChevronDown, ChevronUp } from 'lucide-vue-next';
 
 const { getUnprefixedId } = useIdPrefix();
-
-type Relation = NonNullable<EntityFeature["relations"]>[0]
-& {
-	id: number,
-}
 
 const props = defineProps<{title: string, relations: EntityFeature["relations"]}>();
 
 const actorRelations = computed(() => {
-	return props.relations?.reduce((acc: Array<Relation>, relation) => {
+	const personalRelations = props.relations?.reduce((acc: Array<NonNullable<EntityFeature["relations"]>[0]>, relation) => {
 		if(relation.relationType !== "crm:OA7 has relationship to") return acc;
 		if(!relation.relationTo) return acc;
 		return [
@@ -21,51 +17,43 @@ const actorRelations = computed(() => {
 				id: useToNumber(getUnprefixedId(relation.relationTo)).value
 			}
 		]
-	}, [])
+	}, []);
+
+	return [...groupByToMap(personalRelations ?? [], (rel) => {return rel.type})];
+
 })
 
-const collapsibleRelations = computed(() => {
-	if(!actorRelations.value?.length) return [];
-
-	return actorRelations.value.splice(1)
-})
 
 const isOpen = ref(false)
 </script>
 
 <template v-if="actorRelations?.length">
-	<Collapsible
-    v-model:open="isOpen"
-		class="space-y-2"
-	>
-    <div class="flex items-center justify-between space-x-4 px-4">
-      <h4 class="text-sm font-semibold">
-				{{ title }}
-      </h4>
-      <CollapsibleTrigger as-child>
-        <Button variant="ghost" size="sm" class="w-9 p-0">
-          <ChevronsUpDown class="size-4" />
-          <span class="sr-only">Toggle</span>
-        </Button>
-      </CollapsibleTrigger>
-    </div>
-		<div class="rounded-md border px-4 py-3 text-sm">
-			<!-- TODO: Make to preview Link and Create enity list entry component -->
-			{{ actorRelations?.[0]?.label }}
-		</div>
-		<template v-if="collapsibleRelations.length > 1">
-			<CollapsibleContent class="space-y-2">
-				<template
-					v-for="rel in collapsibleRelations"
-					:key="`actor-rel-${rel.label}`"
-				>
-				<div class="rounded-md border px-4 py-3 text-sm">
-					<EntityPreviewLink :id="rel.id" as-child>
-						{{ rel.label }}
-					</EntityPreviewLink>
-				</div>
-				</template>
+	<div class="rounded-md border px-4 py-3 text-sm">
+		<Collapsible
+			v-model:open="isOpen"
+			class="space-y-2"
+		>
+			<div class="flex items-center justify-between space-x-4">
+				<h4 class="text-sm font-semibold">
+					{{ title }}
+				</h4>
+				<CollapsibleTrigger as-child>
+					<Button variant="ghost" size="sm" class="w-9 p-0">
+						<ChevronDown v-if="isOpen" class="size-4" />
+						<ChevronUp v-else class="size-4" />
+						<span class="sr-only">Toggle</span>
+					</Button>
+				</CollapsibleTrigger>
+			</div>
+			<CollapsibleContent>
+				<RelationCollapsible
+					v-for="[type, rels] in actorRelations"
+					:key="type"
+					class="mb-8"
+					:title="type ?? 'Type'"
+					:relations="rels"
+				/>
 			</CollapsibleContent>
-		</template>
-	</Collapsible>
+		</Collapsible>
+	</div>
 </template>
---
+++ components/relation-list-entry.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const { getUnprefixedId } = useIdPrefix();
+
+defineProps<{relation: NonNullable<EntityFeature["relations"]>[0]}>();
+
+</script>
+
+<template>
+	<div>
+		<Component :is="getEntityIcon(relation.relationSystemClass)" v-if="relation.relationSystemClass" class="mr-1 inline size-5 pb-1" />
+		<EntityPreviewLink :id="useToNumber(getUnprefixedId(relation.relationTo ?? '')).value" as-child>
+			{{ relation.label }}
+		</EntityPreviewLink>
+	</div>
+</template>
