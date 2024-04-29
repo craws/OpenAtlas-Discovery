c46253967efaf7685482745b8b34afe4b2c0009c
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-04-29 16:46:10 +0200

    Add timespan to relation Info

--- components/relation-list-entry.vue
+++ components/relation-list-entry.vue
@@ -6,10 +6,13 @@ defineProps<{relation: NonNullable<EntityFeature["relations"]>[0]}>();
 </script>
 
 <template>
-	<div>
-		<Component :is="getEntityIcon(relation.relationSystemClass)" v-if="relation.relationSystemClass" class="mr-1 inline size-5 pb-1" />
-		<EntityPreviewLink :id="useToNumber(getUnprefixedId(relation.relationTo ?? '')).value" as-child>
-			{{ relation.label }}
-		</EntityPreviewLink>
+	<div class="flex justify-between">
+		<div>
+			<Component :is="getEntityIcon(relation.relationSystemClass)" v-if="relation.relationSystemClass" class="mr-1 inline size-5 pb-1" />
+			<EntityPreviewLink :id="useToNumber(getUnprefixedId(relation.relationTo ?? '')).value" as-child>
+				{{ relation.label }}
+			</EntityPreviewLink>
+		</div>
+		<SimpleTimespan :timespans="relation.when?.timespans" />
 	</div>
 </template>
---
+++ components/simple-timespan.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { isNonEmptyString } from "@acdh-oeaw/lib";
+
+import type { EntityFeature } from "@/composables/use-create-entity";
+
+const t = useTranslations();
+
+const props = defineProps<{
+	timespans: NonNullable<EntityFeature["when"]>["timespans"];
+}>();
+
+const datespans = computed(() => {
+	const datespans: Array<{ start: string | null; end: string | null }> = [];
+
+	props.timespans?.forEach((timespan) => {
+		const _start = timespan.start ? createDateSpan(timespan.start) : null;
+		const _end = timespan.end ? createDateSpan(timespan.end) : null;
+		const start = isNonEmptyString(_start) ? _start : null;
+		const end = isNonEmptyString(_end) ? _end : null;
+		if (start == null && end == null) return;
+		datespans.push({ start, end });
+	});
+
+	return datespans;
+});
+</script>
+
+<template>
+	<div>
+		<template v-if="datespans.length > 0">
+			<template v-for="(timespan, index) of datespans" :key="index">
+				{{timespan.end ? '' : t("TimespansDisplay.start") + ' '}}{{ timespan.start }} {{timespan.start && timespan.end ? '-' : ''}} {{timespan.start ? '' : t("TimespansDisplay.end") + ' '}}{{ timespan.end }}
+			</template>
+		</template>
+		<template v-else>
+			<!-- <div class="italic">{{ t("TimespansDisplay.no-dates") }}</div> -->
+		</template>
+	</div>
+</template>
