c03d9fdf6f0d352f973303fa3217eb25018fe96a
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-07-24 15:34:02 +0200

    fix: call mode-switcher only when an id is provided

--- layouts/visualization.vue
+++ layouts/visualization.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
+import { MapPinIcon, WaypointsIcon } from "lucide-vue-next";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
 const t = useTranslations();
 
 const route = useRoute();
@@ -16,6 +20,13 @@ const id = computed(() => {
 const currentMode = computed(() => {
 	return route.query.mode as string;
 });
+
+function getPath() {
+	if (route.path.includes("visualization")) {
+		return "visualization";
+	}
+	return "";
+}
 </script>
 
 <template>
@@ -25,7 +36,36 @@ const currentMode = computed(() => {
 				<EntitySidebar :id="id" />
 			</template>
 			<div class="absolute right-4 z-20" style="top: calc(50% - 40px)">
-				<ModeSwitch :id="id" :current-mode="currentMode" />
+				<ModeSwitch v-if="id != null" :id="id" :current-mode="currentMode" />
+				<div v-else>
+					<TooltipProvider>
+						<Tooltip>
+							<TooltipTrigger>
+								<div class="rounded-md bg-white/90 p-4 shadow-md dark:bg-neutral-900">
+									<NavLink
+										class="flex items-center gap-1 underline decoration-dotted hover:no-underline"
+										:href="{
+											path: `/${getPath()}`,
+											query: {
+												mode: currentMode === 'network' ? 'map' : 'network',
+											},
+										}"
+									>
+										<MapPinIcon v-if="currentMode === 'network'" class="size-6" />
+										<WaypointsIcon v-else class="size-6" />
+										<span class="sr-only">{{
+											currentMode === "network" ? t("MapPage.title") : t("NetworkPage.title")
+										}}</span>
+									</NavLink>
+								</div>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p v-if="currentMode === 'map'">{{ t("EntityPage.network") }}</p>
+								<p v-if="currentMode === 'network'">{{ t("EntityPage.map") }}</p>
+							</TooltipContent>
+						</Tooltip>
+					</TooltipProvider>
+				</div>
 			</div>
 			<div class="relative grid h-full">
 				<slot />
